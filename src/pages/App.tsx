bdcfbc74d0124440b71def5a826061d9c4c782e1
Darcio Vilela <darcio@jp7.com.br>
2021-08-23 18:25:01 -0300

    List static items

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Header } from '../components/Header';
+
+interface Item {
+  title: string;
+  sendDate: Date;
+}
+
+const items: Item[] = [
+  {
+    title: 'item 1',
+    sendDate: new Date(),
+  },
+  {
+    title: 'item 1',
+    sendDate: new Date(),
+  },
+];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header"> Lista</header>
+      {items.map((item) => (
+        <li>
+          {item.title} - {item.sendDate.toString()}
+        </li>
+      ))}
     </div>
   );
 }
