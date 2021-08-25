fa62996f09a07d71c8232dbdc7ee1737dcbd24dd
Darcio Vilela <darcio@jp7.com.br>
2021-08-25 10:19:55 -0300

    Add json server, axios and create item method

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,35 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Header } from '../components/Header';
-
 import { Item } from '../entities/items';
 
-const items: Item[] = [
-  {
-    title: 'item 1',
-    sendDate: new Date(),
-  },
-  {
-    title: 'item 2',
-    sendDate: new Date(),
-  },
-];
-
 function App() {
+  const [records, setRecords] = useState<Item[]>();
   const [activeRecord, setActiveRecord] = useState<Item>();
+
+  const fetch = async () => {
+    const result = await axios.get<Item[]>('http://localhost:4000/items');
+    setRecords(result.data);
+  };
+
+  const create = async () => {
+    await axios.post<Item>('http://localhost:4000/items', {
+      title: 'item post',
+      sendDate: '2021-09-19T18:56:23.027Z',
+    });
+    fetch();
+  };
+
+  useEffect(() => {
+    fetch();
+  }, []);
+
+  if (!records) return <div>Loading...</div>;
 
   return (
     <div className="App">
       <Header />
       <div>
+        <button onClick={() => create()}>Create New Item</button>
+      </div>
+      <div>
         <h3>ActiveRecord:</h3> {JSON.stringify(activeRecord)}
       </div>
       <div>
         <h3>Items</h3>
-        {items.map((item) => (
-          <li onClick={() => setActiveRecord(item)}>
-            {item.title} - {item.sendDate.toString()}
-          </li>
-        ))}
+        <ul>
+          {records.map((record) => (
+            <li key={record.title} onClick={() => setActiveRecord(record)}>
+              {record.title} - {record.sendDate}
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
